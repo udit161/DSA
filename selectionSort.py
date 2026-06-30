b2bf6b680d35bedce6b213d9fde1134ca6fc779c
Udit Kumar <udit@example.com>
2026-06-30 21:05:43 +0530

    selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,11 @@
+def selectionSort(a):
+    n = len(a)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if a[j] < a[min_idx]:
+                min_idx = j
+        a[i], a[min_idx] = a[min_idx], a[i]
+    return a
+
+print(selectionSort([10, 44, 8, 5, 2, 66]))
