bede1fcc151fede0c9a580c5188e4604a30339d8
Udit Kumar <udit@example.com>
2026-07-01 15:06:22 +0530

    InsertionSort.py

---
+++ InsertionSort.py
@@ -0,0 +1,12 @@
+def insertionSort(a):
+    n = len(a)
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while(j>=0 and a[j] > key):
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+    return a  
+
+print(insertionSort([10, 44, 8, 5, 2, 66]))                 
